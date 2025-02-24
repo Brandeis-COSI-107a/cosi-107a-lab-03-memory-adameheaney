6574a57dc061621a7317cb5cb33fb68ba57639d3
adamheaney <adamheaney@kali.cosi107a.org>
2025-02-23 22:36:35 -0500

    changes

--- print-sizes.c
+++ print-sizes.c
@@ -6,22 +6,22 @@ int main(int argc, char *argv[]) {
     char string[200];
     char *str_point;
 
-    printf("The size of char is %d\n", sizeof(char));
-    printf("The size of short is %d\n", sizeof(short));
-    printf("The size of int is %d\n", sizeof(int));
-    printf("The size of long is %d\n", sizeof(long));
-    printf("The size of long long is %d\n", sizeof(long long));
-    printf("The size of float is %d\n", sizeof(float));
-    printf("The size of double is %d\n", sizeof(double));
-    printf("The size of char * is %d\n", sizeof(char *));
-    printf("The size of int * is %d\n", sizeof(int *));
-    printf("The size of long * is %d\n", sizeof(long *));
+    printf("size of char is %d\n", sizeof(char));
+    printf("size of short is %d\n", sizeof(short));
+    printf("size of int is %d\n", sizeof(int));
+    printf("size of long is %d\n", sizeof(long));
+    printf("size of long long is %d\n", sizeof(long long));
+    printf("size of float is %d\n", sizeof(float));
+    printf("size of double is %d\n", sizeof(double));
+    printf("size of char * is %d\n", sizeof(char *));
+    printf("size of int * is %d\n", sizeof(int *));
+    printf("size of long * is %d\n", sizeof(long *));
 
     printf("The size of my character array is %d\n", sizeof(string));
     str_point = string;
     printf("The size of my character pointer is %d\n", sizeof(str_point));
     val_point = &val;
-    printf("The size of my integer pointer is %d\n", sizeof(val_point));
+    printf("The size of my int pointer is %d\n", sizeof(val_point));
     printf("The size of what my integer pointer points at is %d\n", sizeof(*val_point));
 
 }
